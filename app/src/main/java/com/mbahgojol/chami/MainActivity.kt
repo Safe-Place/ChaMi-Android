2f3d54bad23e2b7e89f394a9b712428e87b05407
knalbdev <profghozimahdi@gmail.com>
2022-05-23 13:17:55 +0700

    test

--- app/src/main/java/com/mbahgojol/chami/MainActivity.kt
+++ app/src/main/java/com/mbahgojol/chami/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.mbahgojol.chami
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.mbahgojol.chami.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHost) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.apply {
+            bottomNavView.setupWithNavController(navController)
+        }
     }
 }
