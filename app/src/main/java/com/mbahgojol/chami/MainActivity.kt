6878b85c453cc36a933de835f25634eb9f86ab40
Annisa <annisarosdiyana17@gmail.com>
2022-05-24 07:39:17 +0700

    Menambahkan mockup untuk halaman home dan halaman detail chat personal

--- app/src/main/java/com/mbahgojol/chami/MainActivity.kt
+++ app/src/main/java/com/mbahgojol/chami/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.mbahgojol.chami
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -23,5 +28,34 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             bottomNavView.setupWithNavController(navController)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search_view, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint_user)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
     }
 }
---
+++ app/src/main/java/com/mbahgojol/chami/ui/main/chat/personal/DetailPersonalChatActivity.kt
@@ -0,0 +1,12 @@
+package com.mbahgojol.chami.ui.main.chat.personal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mbahgojol.chami.R
+
+class DetailPersonalChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_personal_chat)
+    }
+}
