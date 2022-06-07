e8a9e4c7bfcee82867922c32c94a7023223c5a88
Annisa <annisarosdiyana17@gmail.com>
2022-06-08 02:08:09 +0700

    mockup fitur edit profil

---
+++ app/src/main/java/com/mbahgojol/chami/ui/main/others/EditDataActivity.kt
@@ -0,0 +1,12 @@
+package com.mbahgojol.chami.ui.main.others
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mbahgojol.chami.R
+
+class EditDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_data)
+    }
+}
