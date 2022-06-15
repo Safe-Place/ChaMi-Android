6f6fbe9aa2c3cbc21aed4a61ed3115f391c86fd6
Annisa <annisarosdiyana17@gmail.com>
2022-06-16 02:24:22 +0700

    halaman approval

---
+++ app/src/main/java/com/mbahgojol/chami/dummyData/Validasi.kt
@@ -0,0 +1,12 @@
+package com.mbahgojol.chami.dummyData
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Validasi(
+    var avatar: String,
+    var nama: String,
+    var divisi: String,
+    var deskripsi: String,
+) : Parcelable
